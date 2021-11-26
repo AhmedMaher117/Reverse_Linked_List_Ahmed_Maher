96d025abad14227dd36a9c81dcffa98707d93373
Ahmed Maher <Ahmed Maher@LAPTOP-SPU76688>
2021-11-27 01:15:27 +0200

    Add project files.

---
+++ Reverse_Linked_List_Ahmed_Maher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Linked_List_Ahmed_Maher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> MaherList = new LinkedList<string>();
+
+            LinkedListNode<string> MaherNode = MaherList.AddFirst("spiderman3");
+            MaherList.AddFirst("TF");
+            MaherList.AddFirst("Optimus prime");
+            MaherList.AddFirst("Halo");
+            MaherList.AddFirst("Naruto");
+            MaherList.AddFirst("Madara Uchiha");
+            MaherList.AddFirst("Mortal Kombat");
+            MaherList.AddFirst("Scorpion");
+            MaherList.AddBefore(MaherNode, "Kill Zone");
+            MaherList.AddLast("Metal Gear Solid");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("The linked List Before Reversig:  ");
+            Console.WriteLine("----------------------------------");
+            foreach (var item in MaherList)
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("The linked List After Reversing: ");
+            Console.WriteLine("----------------------------------");
+            foreach (var item in MaherList.Reverse())
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Reverse_Linked_List_Ahmed_Maher/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Reverse_Linked_List_Ahmed_Maher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Reverse_Linked_List_Ahmed_Maher")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c6609c2a-e507-46e7-8305-223e1d393e8b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
